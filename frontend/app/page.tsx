aa076a7e81502320b6fe0587f9a5d4d4b3e4e99b
Amandeep <saluja.amandeep9@gmail.com>
2026-04-17 18:19:46 +0530

    modified homepage

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
 
   return (
     <main className="flex flex-col items-center justify-center h-screen gap-6">
-      <h1 className="text-3xl font-bold">ConceptNest</h1>
+      <h1 className="text-3xl font-bold">Welcome to Auzear</h1>
 
       <button
         onClick={() => router.push("/teach")}
