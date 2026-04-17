1c735f8a92c0e6aaac96ff6127e03fa12688723f
Amandeep <saluja.amandeep9@gmail.com>
2026-04-17 16:47:18 +0530

    backend deployment

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,10 @@ const questionRoutes = require("./routes/questionRoutes");
 const insightRoutes = require("./routes/insightRoutes");
 
 app.use("/api", apiLimiter);
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: "*", // for MVP (later restrict)
+}));
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -26,7 +29,7 @@ app.use("/api/revision", revisionRoutes);
 app.use("/api/questions", questionRoutes);
 app.use("/api/insights", insightRoutes);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
